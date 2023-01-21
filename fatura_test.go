8ee8f8dcdb389dbc996d187109ac5c827e430885
ugurkorkmaz <korkmaz.uu@gmail.com>
2023-01-21 23:13:32 +0300

    feat: cretae test last change

--- fatura_test.go
+++ fatura_test.go
@@ -1,1 +1,47 @@
 package fatura_test
+
+import (
+	"fatura"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+var fatura_new = fatura.New()
+
+func TestSetDebug(t *testing.T) {
+	fatura_new.SetDebug(true)
+	if !fatura_new.GetDebug() {
+		t.Error("SetDebug() failed")
+	}
+}
+
+func TestGetDebug(t *testing.T) {
+	fatura_new.SetDebug(true)
+	if !fatura_new.GetDebug() {
+		t.Error("GetDebug() failed")
+	}
+}
+
+func TestSetCridetials(t *testing.T) {
+	f := fatura_new.SetCridetials("username", "password")
+
+	username, password := f.GetCridetials()
+	assert.Equal(t, "username", username)
+	assert.Equal(t, "password", password)
+}
+
+func TestGetCridetials(t *testing.T) {
+	f := fatura_new.SetCridetials("username", "password")
+	username, password := f.GetCridetials()
+	assert.Equal(t, "username", username)
+	assert.Equal(t, "password", password)
+}
+
+func TestGetTestCredentials(t *testing.T) {
+	_, password, err := fatura_new.GetTestCredentials()
+	if err != nil {
+		t.Error("GetTestCredentials() failed")
+	}
+	assert.Equal(t, "1", password)
+}
