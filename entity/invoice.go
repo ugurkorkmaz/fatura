0669cdd6bcdb851cc3d2ce11753a79a999a2ea2c
ugurkorkmaz <korkmaz.uu@gmail.com>
2023-01-21 21:43:29 +0300

    feat: create invoice entity

---
+++ entity/invoice.go
@@ -0,0 +1,74 @@
+package entity
+
+import (
+	"encoding/json"
+	"fatura/entity/enum/arsiv"
+	"fatura/entity/enum/currency"
+	"fatura/entity/enum/invoice"
+)
+
+type Array map[string]interface{}
+
+type Invoice struct {
+	VknTckn                  string        `json:"vknTckn"`
+	AliciAdi                 string        `json:"aliciAdi"`
+	AliciSoyadi              string        `json:"aliciSoyadi"`
+	MahalleSemtIlce          string        `json:"mahalleSemtIlce"`
+	Sehir                    string        `json:"sehir"`
+	Ulke                     string        `json:"ulke"`
+	HangiTip                 arsiv.Type    `json:"hangiTip"`
+	Uuid                     string        `json:"uuid"`
+	BelgeNumarasi            string        `json:"belgeNumarasi"`
+	Tarih                    string        `json:"tarih"`
+	Saat                     string        `json:"saat"`
+	ParaBirimi               currency.Type `json:"paraBirimi"`
+	DovizKuru                float64       `json:"dovizKuru"`
+	FaturaTipi               invoice.Type  `json:"faturaTipi"`
+	SiparisNumarasi          string        `json:"siparisNumarasi"`
+	SiparisTarihi            string        `json:"siparisTarihi"`
+	IrsaliyeNumarasi         string        `json:"irsaliyeNumarasi"`
+	IrsaliyeTarihi           string        `json:"irsaliyeTarihi"`
+	FisNo                    string        `json:"fisNo"`
+	FisTarihi                string        `json:"fisTarihi"`
+	FisSaati                 string        `json:"fisSaati"`
+	FisTipi                  string        `json:"fisTipi"`
+	ZRaporNo                 string        `json:"zRaporNo"`
+	OkcSeriNo                string        `json:"okcSeriNo"`
+	AliciUnvan               string        `json:"aliciUnvan"`
+	Adres                    string        `json:"adres"`
+	BinaAdi                  string        `json:"binaAdi"`
+	BinaNo                   string        `json:"binaNo"`
+	KapiNo                   string        `json:"kapiNo"`
+	KasabaKoy                string        `json:"kasabaKoy"`
+	PostaKodu                string        `json:"postaKodu"`
+	Tel                      string        `json:"tel"`
+	Fax                      string        `json:"fax"`
+	Eposta                   string        `json:"eposta"`
+	Websitesi                string        `json:"websitesi"`
+	VergiDairesi             string        `json:"vergiDairesi"`
+	IadeTable                Array         `json:"iadeTable"`
+	MalHizmetTable           Array         `json:"malHizmetTable"`
+	Not                      string        `json:"not"`
+	Matrah                   float64       `json:"matrah"`
+	MalHizmetToplamTutari    float64       `json:"malHizmetToplamTutari"`
+	ToplamIskonto            float64       `json:"toplamIskonto"`
+	HesaplananKdv            float64       `json:"hesaplananKdv"`
+	VergilerToplami          float64       `json:"vergilerToplami"`
+	VergilerDahilToplamTutar float64       `json:"vergilerDahilToplamTutar"`
+	ToplamMasraflar          float64       `json:"toplamMasraflar"`
+	OdenecekTutar            float64       `json:"odenecekTutar"`
+}
+
+func (i *Invoice) Default() {}
+
+func (i *Invoice) Validate() bool {
+	return true
+}
+
+func (i *Invoice) Json() string {
+	_json, err := json.Marshal(i)
+	if err != nil {
+		return ""
+	}
+	return string(_json)
+}
