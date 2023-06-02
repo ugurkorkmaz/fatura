a563a0e1fda975967b79107a93a2370c29d4ecc3
ugurkorkmaz <korkmaz.uu@gmail.com>
2023-06-02 09:42:14 +0300

    refactor: update for `generic` type of tests

--- array_test.go
+++ array_test.go
@@ -7,33 +7,42 @@ import (
 )
 
 func TestArray(t *testing.T) {
-	arr := make(fatura.Array)
-
-	// Test Set() and Get()
-	arr.Set("name", "John")
-	if arr.Get("name") != "John" {
-		t.Errorf("Expected value: %s, got: %s", "John", arr.Get("name"))
+	arr := fatura.Array[int, string]{
+		1: "apple",
+		2: "banana",
+		3: "orange",
 	}
 
-	// Test Add()
-	arr.Add("phone", "123456789")
-	if len(arr["phone"]) != 1 || arr["phone"][0] != "123456789" {
-		t.Errorf("Expected value: %v, got: %v", []string{"123456789"}, arr["phone"])
-	}
-	arr.Add("phone", "987654321")
-	if len(arr["phone"]) != 2 || arr["phone"][1] != "987654321" {
-		t.Errorf("Expected value: %v, got: %v", []string{"123456789", "987654321"}, arr["phone"])
+	// Get method test
+	if arr.Get(1) != "apple" {
+		t.Error("arr.Get(1) != \"apple\"")
 	}
 
-	// Test Del()
-	arr.Del("name")
-	if arr.Has("name") {
-		t.Errorf("Expected key not to exist: %s", "name")
+	// Set method test
+	arr.Set(1, "pineapple")
+	if arr.Get(1) != "pineapple" {
+		t.Error("arr.Get(1) != \"pineapple\"")
 	}
 
-	// Test Json()
-	expectedJSON := `{"phone":["123456789","987654321"]}`
-	if arr.Json() != expectedJSON {
-		t.Errorf("Expected JSON string: %s, got: %s", expectedJSON, arr.Json())
+	// Del method test
+	arr.Del(1)
+	if arr.Get(1) != "" {
+		t.Error("arr.Get(1) != \"\"")
 	}
+
+	// Has method test
+	if arr.Has(1) {
+		t.Error("arr.Has(1) == true")
+	}
+
+	// Json method test
+	if arr.Json() != "{\"2\":\"banana\",\"3\":\"orange\"}" {
+		t.Error("arr.Json() != \"{\"2\":\"banana\",\"3\":\"orange\"}\"")
+	}
+
+	// Encode method test
+	if arr.Encode() != "2=banana&3=orange" {
+		t.Error("arr.Encode() != \"2=banana&3=orange\"")
+	}
+
 }
